af7c7f432a818b484652c51f6308b1fe06fb5cee
Prince Sharma <142648202+Prince9g@users.noreply.github.com>
2024-11-17 20:26:00 +0530

    Time: 3 ms (18.64%), Space: 10.4 MB (10.67%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        
+        string result;
+        stack<char> st;
+        int start =0;
+        int n=s.size();
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                st.push('(');
+            }
+            else{
+                st.pop();
+            }
+
+            if(st.empty()){
+
+                result += s.substr(start+1, i-start-1);
+                start = i+1;
+            }
+        }
+
+        return result;
+    }
+};
