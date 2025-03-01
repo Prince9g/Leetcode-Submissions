73a84a3d8885abb6eacea948f5b1aa954b8a28a8
Prince Sharma <142648202+Prince9g@users.noreply.github.com>
2025-03-01 21:22:43 +0530

    Time: 0 ms (100%), Space: 13.3 MB (23.58%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0; i<n-1; i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+        }
+        vector<int>ans;
+        for(auto i: nums){
+            if(i > 0) ans.push_back(i);
+        }
+        int zeros = n-ans.size();
+        for(int i=0; i<zeros; i++){
+            ans.push_back(0);
+        }
+        return ans;
+    }
+};
