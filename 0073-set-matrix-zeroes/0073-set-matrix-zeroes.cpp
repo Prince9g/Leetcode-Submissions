79b6718cfc5d20c25afbe334ff6582cf8154cf6e
Prince Sharma <142648202+Prince9g@users.noreply.github.com>
2025-05-21 11:35:52 +0530

    Time: 0 ms (100%), Space: 18.7 MB (52.14%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,70 +1,63 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& mat) {
-        int rows = mat.size();
-        int cols = mat[0].size();
-        bool fcol = false;
-        bool frow = false;
+    void setZeroes(vector<vector<int>>& matrix) {
 
-        // Check if there is a zero in the first column, set fcol to true.
-        for (int i = 0; i < rows; i++) {
-            if (mat[i][0] == 0) {
-                fcol = true;
-                break;
+        // //better appproach 
+        int m = matrix.size();
+        int n = matrix[0].size();
+        // vector<int>rows(m, -1);
+        // vector<int>cols(n, -1);
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(matrix[i][j] == 0) rows[i] = 0;
+        //     }
+        // }
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if(matrix[j][i] == 0) cols[i] = 0;
+        //     }
+        // }
+        // // for(auto i: cols) cout << i << " ";
+        // for(int i=0; i<m; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(rows[i] == 0 || cols[j] == 0){
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+
+
+
+        //best approach now 
+        int col1 = 1;
+        //taking first row and first col as the two vecctors that wwe took as in better approach to optimize space complexity
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    if(j != 0) matrix[0][j] = 0;
+                    else col1 = 0;
+                }
             }
         }
-
-        // Check if there is a zero in the first row, set frow to true.
-        for (int i = 0; i < cols; i++) {
-            if (mat[0][i] == 0) {
-                frow = true;
-                break;
-            }
-        }
-
-        // Check row elements (by ignoring the first row and first column). If
-        // zero is found, set the corresponding row's and column's first element
-        // to zero.
-        for (int i = 1; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
-                if (mat[i][j] == 0) {
-                    mat[0][j] = 0;
-                    mat[i][0] = 0;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                        matrix[i][j] = 0;
+                    }
                 }
             }
         }
 
-        // Check every row's first element starting from the second row.
-        // Set the complete row to zero if zero is found.
-        for (int i = 1; i < rows; i++) {
-            if (mat[i][0] == 0) {
-                for (int j = 1; j < cols; j++) {
-                    mat[i][j] = 0;
-                }
+        if(matrix[0][0] == 0){
+            for(int i=0; i<n; i++){
+                matrix[0][i] = 0;
             }
         }
-
-        // Check every column's first element starting from the second column.
-        // Set the complete column to zero if zero is found.
-        for (int j = 1; j < cols; j++) {
-            if (mat[0][j] == 0) {
-                for (int i = 1; i < rows; i++) {
-                    mat[i][j] = 0;
-                }
-            }
-        }
-
-        // If fcol is true, set the first column to zero.
-        if (fcol) {
-            for (int i = 0; i < rows; i++) {
-                mat[i][0] = 0;
-            }
-        }
-
-        // If frow is true, set the first row to zero.
-        if (frow) {
-            for (int j = 0; j < cols; j++) {
-                mat[0][j] = 0;
+        if(col1 == 0){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
             }
         }
     }
