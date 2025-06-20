f387004b0166d5ed5d2775b70e40126d7cb90496
Prince Sharma <142648202+Prince9g@users.noreply.github.com>
2025-06-20 20:34:32 +0530

    Time: 0 ms (100%), Space: 27.4 MB (8.51%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int>prefix(n), suffix(n);
-        //storing prefix and suffix max for each particular index
-        prefix[0] = height[0], suffix[n-1] = height[n-1];
-        for(int i=1; i<n; i++){
-            prefix[i] = max(prefix[i-1], height[i]);
-        }
-        for(int i=n-2; i>=0; i--){
-            suffix[i] = max(suffix[i+1], height[i]);
-        }
-        //looping to  all and initializes the variables for returning the answers
-        int total = 0;
+    int trap(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>prefixMax(n), suffixMax(n);
+        prefixMax[0] = nums[0];
+        suffixMax[n-1] = nums[n-1];
+        for(int i=1; i<n; i++) prefixMax[i] = max(prefixMax[i-1], nums[i]);
+        for(int i=n-2; i>=0; i--) suffixMax[i] = max(suffixMax[i+1], nums[i]);
+        int t = 0;
         for(int i=0; i<n; i++){
-            
-                total += min(prefix[i], suffix[i]) - height[i];
-        
+            if(nums[i] < prefixMax[i] && nums[i] < suffixMax[i]){
+                t += min(prefixMax[i], suffixMax[i]) - nums[i];
+            }
         }
-        return total;
+        return t;
     }
 };
